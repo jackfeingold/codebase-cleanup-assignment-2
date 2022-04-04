0f2d723f613a8a261f03c54b63022260d72bd648
jackfeingold <jpf99@georgetown.edu>
2022-04-03 23:05:32 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -5,9 +5,11 @@ def to_usd(my_price):
 
     """
     This function will format a number that is passed into the function as USD
-    It will be comma separated and formatted as $0.00
+    It will be comma separated and formatted as $0.00. 
+    The function requires a float or integer to be put in and will return a string.
 
     Invoke like this: to_usd(9.99999)
+    The expected output will be "$10.00"
     """
 
 
