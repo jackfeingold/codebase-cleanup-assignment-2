5d7c6eeb5609a67de3ab8bef9670b33185a1a7f7
jackfeingold <jpf99@georgetown.edu>
2022-04-04 22:22:41 -0400

    Delete test_alphavantage_service.py

--- tests/test_alphavantage_service.py
+++
@@ -1,8 +0,0 @@
-
-from app.alphavantage_service import fetch_data_crypto, fetch_data_stocks, fetch_data_unemployment
-from pprint import pprint
-
-
-#pprint(fetch_data("BTC"))
-
-pprint(fetch_data_unemployment())["data"]
